c16c352668b6e7d593582c067ab13a4fb0d11f84
Eleonora Cappuccio <eleonora.cappuccio@phd.unipi.it>
2026-09-19 15:12:30 +0200

    Cambia lo stile della mappa in CartoDB Positron

--- js/map.js
+++ js/map.js
@@ -19,9 +19,10 @@
     maxZoom: SITE_CONFIG.MAP_MAX_ZOOM,
   }).setView(SITE_CONFIG.MAP_CENTER, SITE_CONFIG.MAP_ZOOM);
 
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
     attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: "abcd",
     maxZoom: SITE_CONFIG.MAP_MAX_ZOOM,
   }).addTo(map);
 
